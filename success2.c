de76bc237c25a8669a44f01fe108abb6310569a7
Hans T Walheim <hans.walheim@gmail.com>
2020-01-02 18:03:14 +0100

    First trial

---
+++ success2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+extern int the_ladder_to_success3(int in);
+
+int the_ladder_to_success2(int in)
+{
+  printf ("You are at the second stage on the ladder to success\n");
+
+  return the_ladder_to_success3(in);
+}
